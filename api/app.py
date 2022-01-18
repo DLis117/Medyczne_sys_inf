f8fb7d95629853f643b05b3afeaf5caee85a00b8
DLis117 <domills117@gmail.com>
2022-01-18 21:04:09 +0100

    empty flask+sqlite3+docker app

---
+++ api/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template,request,redirect,url_for,flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, LoginManager,login_user, current_user,logout_user,login_required
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app=Flask(__name__)
+db=SQLAlchemy()
+login_manager=LoginManager()
+
+class User(UserMixin,db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(25))
+    password=db.Column(db.String(100))
+
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route("/")
+def main():
+    return ("hello flask!")
+
+if __name__=="__main__":
+    app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///db.sqlite'
+    app.config['SECRET_KEY']='XDDDDD'
+    db.init_app(app)
+    login_manager.login_view='login'
+    login_manager.init_app(app)
+    db.create_all(app=app)
+    app.run()
